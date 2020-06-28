b6e08d523b5acabb74d3c8b13e088b104d9ac6ac
Rohit Saini <sketchlearn7@gmail.com>
2020-06-28 14:25:33 +0530

    first commit

---
+++ SwiftUI2/BookDetail.swift
@@ -0,0 +1,21 @@
+//
+//  BookDetail.swift
+//  SwiftUI2
+//
+//  Created by Rohit Saini on 28/06/20.
+//  Copyright © 2020 AccessDenied. All rights reserved.
+//
+
+import SwiftUI
+
+struct BookDetail: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct BookDetail_Previews: PreviewProvider {
+    static var previews: some View {
+        BookDetail()
+    }
+}
---
+++ SwiftUI2/Model/Book.swift
@@ -0,0 +1,9 @@
+//
+//  Book.swift
+//  SwiftUI2
+//
+//  Created by Rohit Saini on 28/06/20.
+//  Copyright © 2020 AccessDenied. All rights reserved.
+//
+
+import Foundation
